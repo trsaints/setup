bee8e1c1e5c66ba904833dc263bfd41ca06b5b61
Thiago Rodrigues <thiago2003.trs@gmail.com>
2022-03-07 11:49:05 -0300

    Hour formatting has been done properly (Shows "AM" or "PM" according the moment of the day);

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 // SELECT DOM ELEMENTS
-const 
+const
   date = document.querySelector(".date"),
   time = document.querySelector(".time"),
   greetingTime = document.querySelector(".greeting-time"),
-  userName = document.getElementById("userName"),
+  userName = document.querySelector("#userName"),
   focusAnswer = document.querySelector(".focus-answer");
 
 // Set and show time
@@ -16,9 +16,20 @@ function showTime() {
   // format time (e.g. showing 08:00 PM instead of 20:00 PM)
   hours > 12 ? hours = hours % 12 : hours = today.getHours();
 
+  // show "AM" after time if it's morning, otherwise show "PM" instead.
+  function periodOfDay() {
+    let hoursPeriod;
+    if (hours < 12) {
+      hoursPeriod = "A.M.";
+    } else {
+      hoursPeriod = "P.M.";
+    }
+    return hoursPeriod;
+  }
+
   // output time
   time.innerHTML = `
-      ${String(hours).padStart(2, 0)}:${String(minutes).padStart(2, 0)}:${String(seconds).padStart(2, 0)}`;
+      ${String(hours).padStart(2, 0)}:${String(minutes).padStart(2, 0)}:${String(seconds).padStart(2, 0)} ${periodOfDay()}`;
 
   setInterval(showTime, 1000);
 }
@@ -28,19 +39,11 @@ function showDate() {
   let today = new Date(),
     year = today.getFullYear(),
     day = today.getDate(),
-    weekDay = today.toLocaleDateString("default", { weekday: "long" }),
-    month = today.toLocaleString("default", { month: "long" });
+    weekDay = today.toLocaleDateString("pt-br", { weekday: "short" }),
+    month = today.toLocaleString("pt-br", { month: "long" });
   if (date) {
     date.innerHTML = `${String(weekDay)}, ${String(day)} de ${String(month)} de ${year}`;
   }
-}
-
-// Set userName input to fit its content's width (according user name's character size)
-userName.addEventListener('input', resizeInput);
-resizeInput.call(userName);
-
-function resizeInput() {
-  this.style.width = this.value.length + "ch";
 }
 
 // Set background and greeting text according time period of the day
@@ -50,23 +53,23 @@ function periodBackground() {
     periodHour = dayPeriod.getHours();
 
   if (periodHour < 12) {
-    document.body.style.backgroundImage = 'url("./img/morning.jpg")';
+    document.body.style.background = 'linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2) 100%), url("./img/morning.jpg")';
     greetingTime.innerHTML = `
-      Bom dia, ${userName}.
+      <p>Bom dia, <input type="text" id="userName" class="user-name"></input>.
       <br />
-      Qual é o seu foco hoje?`
+      Qual é o seu foco hoje?</p>`
   } else if (periodHour > 12 || periodHour < 18) {
-    document.body.style.backgroundImage = 'url("./img/afternoon.jpg")';
+    document.body.style.background = 'linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2) 100%), url("./img/afternoon.jpg")';
     greetingTime.innerHTML = `
-      Boa tarde, ${userName}.
+      <p>Boa tarde, <input type="text" id="userName" class="user-name"></input>.
       <br />
-      Qual é o seu foco hoje?`
+      Qual é o seu foco hoje?</p>`
   } else {
-    document.body.style.backgroundImage = 'url("./img/night.jpg")';
+    document.body.style.background = 'linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2) 100%), url("./img/night.jpg")';
     greetingTime.innerHTML = `
-      Boa noite, ${userName}.
+      <p>Boa noite, <input type="text" id="userName" class="user-name"></input>.
       <br />
-      Qual é o seu foco hoje?`
+      Qual é o seu foco hoje?</p>`
   }
 }
 // run 
